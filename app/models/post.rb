77131a03abe7082f9183ea110bfb0d575461c431
Daniel Williams <danielpaulwill@gmail.com>
2022-06-13 19:04:24 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :title_is_click_bait
+
+  title = self.title
+  puts title
+
+  def title_is_click_bait
+    title = self.title
+    if title.where('title.include = ?', "Won't Believe" || "Secret" || "Top [number]" || "Guess")
+      puts "all clear"
+    else
+      errors.add(:title, "Not click bait-y enough!")
+  end
+  
 end
